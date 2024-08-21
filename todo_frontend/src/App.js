35bc6f08122dadfce003245f2e406971aa862aa0
Daniel Eichman <d.eichman@outlook.com>
2024-08-21 00:13:35 -0700

    Convert project to use functional components and useState

--- todo_frontend/src/App.js
+++ todo_frontend/src/App.js
@@ -1,101 +1,93 @@
-import React, { Component } from "react";
-import Modal from "./components/Modal";
+import React, {useEffect, useState} from "react";
+import {CustomModal} from "./components/Modal";
 import axios from "axios";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewCompleted: false,
-      todoList: [],
-      modal: false,
-      activeItem: {
-        title: "",
-        description: "",
-        completed: false,
-      },
-      message: "",
-      response: "",
-    };
-  }
+function App() {
+  const [state, setState] = useState({
+    viewCompleted: false,
+    todoList: [],
+    modal: false,
+    activeItem: {
+      title: "",
+      description: "",
+      completed: false,
+    },
+    message: "",
+    response: "",
+  });
 
-  componentDidMount() {
-    this.refreshList();
-  }
+  useEffect(() => {
+    refreshList();
+  }, [])
 
-  refreshList = () => {
+  const refreshList = () => {
     axios
       .get("/api/tasks/")
-      .then((res) => this.setState({ todoList: res.data }))
+      .then((res) => setState(prev => ({...prev, todoList: res.data})))
       .catch((err) => console.log(err));
+  }
+
+  const toggle = () => {
+    setState((prev) => ({...prev, modal: !prev.modal}));
   };
 
-  toggle = () => {
-    this.setState({ modal: !this.state.modal });
-  };
-
-  handleSubmit = (item) => {
-    this.toggle();
+  const handleSubmit = (item) => {
+    toggle();
 
     if (item.id) {
       axios
         .put(`/api/tasks/${item.id}/`, item)
-        .then((res) => this.refreshList());
+        .then((res) => refreshList());
       return;
     }
     axios
       .post("/api/tasks/", item)
-      .then((res) => this.refreshList());
+      .then((res) => refreshList());
   };
 
-  handleChatSubmit = (e) => {
+  const handleChatSubmit = (e) => {
     e.preventDefault();
 
     try {
       axios
-        .post('http://127.0.0.1:8000/api/generate-text/', { prompt: this.state.message })
-        .then((res) => this.setState({ response: res.data["data"] }));
+        .post('http://127.0.0.1:8000/api/generate-text/', {prompt: state.message})
+        .then((res) => setState(prev => ({...prev, response: res.data["data"]})));
     } catch (error) {
       console.error('Error chatting with GPT:', error);
     }
-  };  
+  };
 
-  handleDelete = (item) => {
+  const handleDelete = (item) => {
     axios
       .delete(`/api/tasks/${item.id}/`)
-      .then((res) => this.refreshList());
+      .then((res) => refreshList());
   };
 
-  createItem = () => {
-    const item = { title: "", description: "", completed: false };
-
-    this.setState({ activeItem: item, modal: !this.state.modal });
+  const createItem = () => {
+    const item = {title: "", description: "", completed: false};
+    setState((prev) => ({...prev, activeItem: item, modal: !prev.modal}));
   };
 
-  editItem = (item) => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
+  const editItem = (item) => {
+    setState((prev) => ({...prev, activeItem: item, modal: !prev.modal}));
   };
 
-  displayCompleted = (status) => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
-    }
-
-    return this.setState({ viewCompleted: false });
+  const displayCompleted = (status) => {
+    setState((prev) => ({...prev, viewCompleted: status}));
   };
 
-  renderTabList = () => {
+  const renderTabList = () => {
     return (
       <div className="nav nav-tabs">
         <span
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => displayCompleted(true)}
+          className={state.viewCompleted ? "nav-link active" : "nav-link"}
         >
           Complete
         </span>
         <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
+          onClick={() => displayCompleted(false)}
+          className={state.viewCompleted ? "nav-link" : "nav-link active"}
         >
           Incomplete
         </span>
@@ -103,9 +95,9 @@ class App extends Component {
     );
   };
 
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
+  const renderItems = () => {
+    const {viewCompleted} = state;
+    const newItems = state.todoList.filter(
       (item) => item.completed === viewCompleted
     );
 
@@ -116,7 +108,7 @@ class App extends Component {
       >
         <span
           className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
+            state.viewCompleted ? "completed-todo" : ""
           }`}
           title={item.description}
         >
@@ -125,13 +117,13 @@ class App extends Component {
         <span>
           <button
             className="btn btn-secondary mr-2"
-            onClick={() => this.editItem(item)}
+            onClick={() => editItem(item)}
           >
             Edit
           </button>
           <button
             className="btn btn-danger"
-            onClick={() => this.handleDelete(item)}
+            onClick={() => handleDelete(item)}
           >
             Delete
           </button>
@@ -140,57 +132,55 @@ class App extends Component {
     ));
   };
 
-  render() {
-    return (
-      <main className="container">
-        <h1 className="text-uppercase text-center my-4">Todo app</h1>
-        <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <div className="mb-4">
-                <button
-                  className="btn btn-primary"
-                  onClick={this.createItem}
-                >
-                  Add task
-                </button>
-              </div>
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush border-top-0">
-                {this.renderItems()}
-              </ul>
+  return (
+    <main className="container">
+      <h1 className="text-uppercase text-center my-4">Todo app</h1>
+      <div className="row">
+        <div className="col-md-6 col-sm-10 mx-auto p-0">
+          <div className="card p-3">
+            <div className="mb-4">
+              <button
+                className="btn btn-primary"
+                onClick={() => createItem()}
+              >
+                Add task
+              </button>
             </div>
+            {renderTabList()}
+            <ul className="list-group list-group-flush border-top-0">
+              {renderItems()}
+            </ul>
           </div>
         </div>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ) : null}
-        <div className="row text-center">
-          <h2>Summarize tasks with GPT</h2>
-          <form onSubmit={this.handleChatSubmit}>
+      </div>
+      {state.modal ? (
+        <CustomModal
+          activeItem={state.activeItem}
+          toggle={() => toggle()}
+          onSave={(task) => handleSubmit(task)}
+        />
+      ) : null}
+      <div className="row text-center">
+        <h2>Summarize tasks with GPT</h2>
+        <form onSubmit={(e) => handleChatSubmit(e)}>
             <textarea
-              value={this.state.message}
-              onChange={(e) => this.setState({ message: e.target.value })}
+              value={state.message}
+              onChange={(e) => setState((prev) => ({...prev, message: e.target.value}))}
               rows="4"
               cols="50"
             />
-            <br/>
-            <button className="btn btn-primary" type="submit">Send</button>
-          </form>
-          {this.state.response && (
-            <div>
-              <h2>Response:</h2>
-              <p>{this.state.response}</p>
-            </div>
-          )}
-        </div>        
-      </main>
-    );
-  }
+          <br/>
+          <button className="btn btn-primary" type="submit">Send</button>
+        </form>
+        {state.response && (
+          <div>
+            <h2>Response:</h2>
+            <p>{state.response}</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- todo_frontend/src/components/Modal.js
+++ todo_frontend/src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import {
   Button,
   Modal,
@@ -11,78 +11,57 @@ import {
   Label,
 } from "reactstrap";
 
-export default class CustomModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeItem: this.props.activeItem,
-    };
-  }
+export function CustomModal(props) {
+  const [modalState, setModalState] = useState(props.activeItem);
 
-  handleChange = (e) => {
-    let { name, value } = e.target;
-
-    if (e.target.type === "checkbox") {
-      value = e.target.checked;
-    }
-
-    const activeItem = { ...this.state.activeItem, [name]: value };
-
-    this.setState({ activeItem });
-  };
-
-  render() {
-    const { toggle, onSave } = this.props;
-
-    return (
-      <Modal isOpen={true} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
-        <ModalBody>
-          <Form>
-            <FormGroup>
-              <Label for="todo-title">Title</Label>
+  return (
+    <Modal isOpen={true} toggle={props.toggle}>
+      <ModalHeader toggle={props.toggle}>Todo Item</ModalHeader>
+      <ModalBody>
+        <Form>
+          <FormGroup>
+            <Label for="todo-title">Title</Label>
+            <Input
+              type="text"
+              id="todo-title"
+              name="title"
+              value={modalState.title}
+              onChange={(e) => setModalState(prev => ({...prev, title: e.target.value}))}
+              placeholder="Enter Todo Title"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="todo-description">Description</Label>
+            <Input
+              type="text"
+              id="todo-description"
+              name="description"
+              value={modalState.description}
+              onChange={(e) => setModalState(prev => ({...prev, description: e.target.value}))}
+              placeholder="Enter Todo description"
+            />
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
               <Input
-                type="text"
-                id="todo-title"
-                name="title"
-                value={this.state.activeItem.title}
-                onChange={this.handleChange}
-                placeholder="Enter Todo Title"
+                type="checkbox"
+                name="completed"
+                checked={modalState.completed}
+                onChange={(e) => setModalState(prev => ({...prev, completed: e.target.checked}))}
               />
-            </FormGroup>
-            <FormGroup>
-              <Label for="todo-description">Description</Label>
-              <Input
-                type="text"
-                id="todo-description"
-                name="description"
-                value={this.state.activeItem.description}
-                onChange={this.handleChange}
-                placeholder="Enter Todo description"
-              />
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input
-                  type="checkbox"
-                  name="completed"
-                  checked={this.state.activeItem.completed}
-                  onChange={this.handleChange}
-                />
-                Completed
-              </Label>
-            </FormGroup>
-          </Form>
-        </ModalBody>
-        <ModalFooter>
-          <Button
-            color="success"
-            onClick={() => onSave(this.state.activeItem)}
-          >
-            Save
-          </Button>
-        </ModalFooter>
-      </Modal>
-    );
-  }
+              Completed
+            </Label>
+          </FormGroup>
+        </Form>
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          color="success"
+          onClick={() => props.onSave(modalState)}
+        >
+          Save
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 }
